2b118a6c1567620a73e2b8636551c68c96cf2f7a
Sersid <sersONEd@gmail.com>
2015-03-26 05:04:16 +0300

    Init

---
+++ Asset.php
@@ -0,0 +1,24 @@
+<?php
+namespace sersid\owlcarousel;
+
+use yii\web\AssetBundle;
+
+class Asset extends AssetBundle
+{
+    public $sourcePath = '@bower/owlcarousel/owl-carousel';
+
+    public $css = [
+        'owl.carousel.css',
+        'owl.theme.css',
+    ];
+
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+    public function registerAssetFiles($view)
+    {
+        $this->js[] = 'owl.carousel' . (!YII_DEBUG ? '.min' : '') . '.js';
+        parent::registerAssetFiles($view);
+    }
+}
